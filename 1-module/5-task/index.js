51ef207c6cceee47fd0f31c87ef0dbe07935e435
Natallia Markava <natallia.markava@newland.by>
2022-06-10 16:20:24 +0300

    5th task. cut a string

--- 1-module/5-task/index.js
+++ 1-module/5-task/index.js
@@ -1,3 +1,25 @@
+/*
+Первоначальные проверки:
+  
+  1. Предполагаем, что работаем только со строками без преобразования
+  2. Переданные данные - строка
+  3. Возвращается та же строка, если усечение не требуется
+  4. Возвращается усеченная строка, если длина исходной строки больше maxlength
+  
+*/
+
 function truncate(str, maxlength) {
-  // ваш код...
+
+  let resultStr;
+
+  (str.length <= maxlength) ? resultStr = str : resultStr = str.slice(0, maxlength - 1) + "…";
+
+
+  return resultStr;
+  // console.log(str.length);
+  // console.log(resultStr);
+
 }
+
+
+truncate('обрезаем символы строки', 4);
