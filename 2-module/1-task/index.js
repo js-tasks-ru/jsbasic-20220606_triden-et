a5262c911ccee5ea5f6aa62328dd2bd5e0ecd565
Natallia Markava <natallia.markava@newland.by>
2022-06-15 15:59:58 +0300

    2 module. 1st task. sumSalary function

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,13 @@
 function sumSalary(salaries) {
-  // ваш код...
+  let teamSalaries = 0;
+
+  for (key in salaries) {
+
+      if (typeof salaries[key] === "number" && salaries[key] > 0 && salaries[key] !== Infinity) {
+
+              teamSalaries = teamSalaries + salaries[key];
+      }
+  }
+  
+    return teamSalaries; 
 }
