de482913056d514c779acda3520f6a3b2c000d2b
Natallia Markava <natallia.markava@newland.by>
2022-06-20 16:16:13 +0300

    module 2 task 3 calculator

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,6 +1,26 @@
-let calculator = {
-  // ваш код
-};
 
-// НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
-window.calculator = calculator; // делает ваш калькулятор доступным глобально
+// let first = +prompt('Введите первое число');
+// let second = +prompt('Введите второе число');
+
+// // calculator.read;
+
+
+let calculator = {
+
+  read: function(first, second) {
+    
+    this.first = first;
+    this.second = second;    
+  },
+
+  sum: function() {
+    return this.first + this.second;
+  },
+
+  mul: function() {
+    return this.first * this.second;
+  },
+}
+
+// calculator.read();
+// console.log(calculator);
