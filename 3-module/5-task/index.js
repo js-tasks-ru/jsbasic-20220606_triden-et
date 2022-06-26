e4f7d43948cac2a361e28b3ffea4def3fba8c9a7
natcet <natalymarkovam@gmail.com>
2022-06-26 11:56:24 +0300

    Module 3. Task 5. Min, mmax function

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,24 @@
 function getMinMax(str) {
-  // ваш код...
-}
+  let arr = str.split(' ');
+
+  let isNumber = (strg) => {
+      if ( isNaN(strg) === false ) {
+          // console.log(str);
+      return +strg;
+      }
+  }
+
+  let arrMap = arr.map(isNumber);
+  let arrFilter = arrMap.filter( stg => stg !== undefined );
+
+  let maxValueOfArr = Math.max.apply(null, arrFilter);
+  let minValueOfArr = Math.min.apply(null, arrFilter);
+
+  let result = {
+      min: minValueOfArr,
+      max: maxValueOfArr,
+  }
+
+  return result;
+
+  }
