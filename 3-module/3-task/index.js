2dffcc3b8912fa591fd311dd25667f9b71555d12
natcet <natalymarkovam@gmail.com>
2022-06-26 15:42:31 +0300

    Module 3.Task 3. Camelizefunction

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,22 @@
 function camelize(str) {
-  // ваш код...
+  let arr = str.split('-');
+
+  let fooUpperLetter = (strg) => {
+      let upperString = strg.slice(0, 1).toUpperCase() + strg.slice(1);
+      return upperString;
+  }
+
+  let arrToUppCase = arr.map(fooUpperLetter);
+
+  let arrAllUpperCase = arrToUppCase.join('');
+
+  let strCamelize;
+
+  if (str.slice(0, 1) === '-') {
+    strCamelize = arrAllUpperCase;
+  } else {
+    strCamelize = arrAllUpperCase.slice(0, 1).toLowerCase() + arrAllUpperCase.slice(1);
+  }
+
+  return strCamelize;
 }
